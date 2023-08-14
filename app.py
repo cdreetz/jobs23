2c23c3b06e8a6547f425c9d4ea027f9e94a729a4
cdreetz <cdreetz@gmail.com>
2023-08-13 20:20:57 -0500

    initial

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:topromen@localhost/jobs_db'
+db = SQLAlchemy(app)
+
+# Database models
+class Job(db.Model):
+    job_id = db.Column(db.String, primary_key=True)
+    role_name = db.Column(db.String, nullable=False)
+    company_name = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+class Skill(db.Model):
+    skill_id = db.Column(db.Integer, primary_key=True)
+    skill_name = db.Column(db.String, nullable=False)
+    mention_count = db.Column(db.Integer, default=0)
+
+
+# List of potential skills - this can be expanded upon
+SKILLS = [
+    "python", "r", "sql", "machine learning", "deep learning", "tensorflow", 
+    "pytorch", "tableau", "excel", "big data", "hadoop", "spark", "statistics", 
+    "nlp", "natural language processing", "computer vision"
+]
+
+def extract_skills_from_description(description):
+    for skill in SKILLS:
+        if skill in description.lower():
+            # Check if skill already exists in the database
+            skill_entry = Skill.query.filter_by(skill_name=skill).first()
+            if skill_entry:
+                skill_entry.mention_count += 1
+            else:
+                new_skill = Skill(skill_name=skill, mention_count=1)
+                db.session.add(new_skill)
+            db.session.commit()
+
+
+
+
+
+
+
+
+
+
+@app.route('/skills', methods=['GET'])
+def get_skills():
+    skills = Skill.query.order_by(Skill.mention_count.desc()).all()
+    result = [{'skill_name': s.skill_name, 'mention_count': s.mention_count} for s in skills]
+    return jsonify(result)
+
+
+@app.route('/compare-skills', methods=['POST'])
+def compare_skills():
+    user_skills = request.form.getlist('skills')  # Get list from form checkboxes
+    
+    matched_skills = Skill.query.filter(Skill.skill_name.in_(user_skills)).all()
+    
+    top_skills = Skill.query.order_by(Skill.mention_count.desc()).limit(10).all()
+    user_top_skills = [s for s in top_skills if s.skill_name in user_skills]
+    
+    return render_template('results.html', 
+                           total_top_skills=len(top_skills), 
+                           user_top_skills_count=len(user_top_skills), 
+                           user_top_skills=[s.skill_name for s in user_top_skills])
+
+
+@app.route('/input-skills', methods=['GET'])
+def input_skills():
+    skills = Skill.query.all()
+    return render_template('input_skills.html', skills=skills)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+
+
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ scraper.py
@@ -0,0 +1,124 @@
+import json
+from scrapers.Bot import Bot
+from selenium.webdriver.common.by import By
+from time import sleep
+import itertools
+import urllib
+from selenium.common.exceptions import NoSuchElementException
+import os
+from random import shuffle
+
+
+class StackScraper(Bot):
+    def __init__(self):
+        super().__init__(verbose=True)
+
+        role_names = [
+            "data scientist",
+            "data analyst",
+            "data engineer",
+            "machine learning engineer"
+        ]
+        companies = [
+            "apple",
+            "google",
+            "microsoft",
+            "facebook",
+            "tesla",
+            "amazon",
+            "UT Health Science Center at San Antonio",
+            #"HEB"
+        ]
+        shuffle(role_names)
+        self.driver.get("https://www.google.com")
+        for role_name, company in itertools.product(role_names, companies):
+            self.get_all_jobs(role_name, company)
+
+    def get_all_jobs(self, role_name, company):
+        query = f"https://www.google.com/search?q={role_name} {company}&ibp=htl;jobs#htivrt=jobs".replace(
+            ' ', '+')
+        print(query)
+        self.driver.get(query)
+        listings = self.driver.find_elements(
+            By.XPATH, "//div[@class='PwjeAc']")
+        print(f"Number of listings found: {len(listings)}")
+        sleep(1)
+        for idx, listing in enumerate(listings):
+            self.scroll_into_view(listing)
+            # id = listing.find_element(By.XPATH, '..').get_attribute("data-ved")
+            # print(id)
+            listing.click()
+            # print("Click listing")
+            sleep(0.5)
+            try:
+                job = self._get_job()
+            except:
+                continue
+            self.save_job(job, role_name, company)
+
+    def scroll_into_view(self, element):
+        try:
+            self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+        except Exception as e:
+            print(f"Error while scrolling to element: {e}")
+                
+        
+    def _get_job(self):
+        return {
+            "id": self._get_job_id(),
+            "company": self._get_company(),
+            "description": self._get_job_description()
+        }
+    
+    def _get_job_id(self):
+        parsed_url = urllib.parse.urlparse(self.driver.current_url)
+        id = urllib.parse.parse_qs(parsed_url.fragment)['htidocid'][0]
+        return id
+    
+    def _get_company(self):
+        job_container = self.driver.find_element(
+            By.XPATH, '//div[@class="whazf bD1FPe"]') 
+        company = job_container.find_element(
+            By.XPATH, './/div[@class="nJlQNd sMzDkb"]'
+            ).text
+        return company
+    
+    def _get_job_description(self):
+        job_container = self.driver.find_element(
+            By.XPATH, '//div[@class="whazf bD1FPe"]')
+        try:
+            expand_description_button = job_container.find_element(
+                By.XPATH, 'div/div/div/div/div/div/div[@class="CdXzFe j4kHIf"]')
+            self.scroll_into_view(expand_description_button)
+            expand_description_button.click()
+        except NoSuchElementException:
+            pass
+        description = job_container.find_element(
+            By.XPATH, ".//span[@class='HBvzbc']").text
+        # By.XPATH "div/div/div/div/div/span[@class=']".text
+        return description
+    
+    def save_job(self, job, role_name, company):
+        # Check if job already exists in the database
+        existing_job = Job.query.filter_by(job_id=job["id"]).first()
+        
+        if not existing_job:
+            new_job = Job(
+                job_id=job["id"],
+                role_name=role_name,
+                company_name=company,
+                description=job["description"]
+            )
+            db.session.add(new_job)
+            db.session.commit()
+
+
+
+if __name__ == '__main__':
+    StackScraper()
+        
+
+        
+    
+
+        
